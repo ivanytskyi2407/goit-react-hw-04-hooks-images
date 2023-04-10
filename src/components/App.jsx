fef54162ab9642c2ca7780bf73a8f47aed1da6ae
Oleh Ivanytskyi <89008107+ivanytskyi2407@users.noreply.github.com>
2023-04-10 15:25:52 +0300

    refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,3 @@
-// import React, { Component } from 'react';
 import { ToastContainer } from 'react-toastify';
 import s from './App.module.css';
 import 'react-toastify/dist/ReactToastify.min.css';
@@ -8,9 +7,7 @@ import API from './API/API';
 import { toast } from 'react-toastify';
 import ImageGallery from './ImageGallery/ImageGallery';
 import Button from './Button/Button';
-///////////////////////////////////////////////
 import { useState, useEffect } from 'react';
-
 const App = () => {
   const [query, setQuery] = useState(null);
   const [page, setPage] = useState(1);
@@ -25,7 +22,6 @@ const App = () => {
     setLoader(true);
     API(query, page)
       .then(({ hits, totalHits }) => {
-        console.log(hits);
         if (hits < 12) {
           setLoadMore(false);
         }
@@ -66,67 +62,3 @@ const App = () => {
   );
 };
 export default App;
-
-////////////////////////////////////////////////////////
-
-// export default class App extends Component {
-//   state = {
-//     query: '',
-//     loader: false,
-//     page: 1,
-//     pictures: [],
-//     loadMore: false,
-//   };
-
-//   componentDidUpdate(prevProps, prevState) {
-//     const { query, page } = this.state;
-//     if (prevState.query !== query || prevState.page !== page) {
-//       this.setState({ loader: true });
-
-//       API(query, page).then(pictures => {
-//         this.setState({ loader: false });
-//         if (pictures.length === 0) {
-//           return toast.error('Нічого немає');
-//         }
-
-//         this.setState(prevState => ({
-//           pictures: [...prevState.pictures, ...pictures],
-//           loadMore: true,
-//         }));
-//         if (pictures.length < 12) {
-//           this.setState({
-//             loadMore: false,
-//           });
-//         }
-//       });
-//     }
-//   }
-//   handlFormSubmit = query => {
-//     this.setState({ query });
-//     this.defaultState();
-//   };
-
-//   onClickButton = () => {
-//     this.setState(prevState => {
-//       return {
-//         page: prevState.page + 1,
-//       };
-//     });
-//   };
-//   defaultState = () => {
-//     this.setState({ page: 1, pictures: [] });
-//   };
-
-//   render() {
-//     const { loader, pictures, loadMore } = this.state;
-//     return (
-//       <div className={s.App}>
-//         <Searchbar onSubmit={this.handlFormSubmit} />
-//         {loader && <Loader />}
-//         <ImageGallery pictures={pictures}></ImageGallery>
-//         {loadMore && <Button onClickButton={this.onClickButton} />}
-//         <ToastContainer autoClose={2000} position="top-center" />
-//       </div>
-//     );
-//   }
-// }
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { toast } from 'react-toastify';
 import s from './Searchbar.module.css';
-// import { Component } from 'react';
 
 const Searchbar = ({ onSubmit }) => {
   const [query, setQuery] = useState('');
@@ -37,44 +36,3 @@ const Searchbar = ({ onSubmit }) => {
   );
 };
 export default Searchbar;
-
-// export default class Searchbar extends Component {
-//   state = {
-//     query: '',
-//   };
-
-//   handleQueryChange = e => {
-//     this.setState({ query: e.currentTarget.value.toLowerCase() });
-//   };
-
-//   handleSubmit = e => {
-//     e.preventDefault();
-//     if (this.state.query.trim() === '') {
-//       return toast.error('Введіть назву картинки');
-//     }
-//     this.props.onSubmit(this.state.query);
-//     this.setState({ query: '' });
-//   };
-
-//   render() {
-//     return (
-//       <header className={s.Searchbar}>
-//         <form className={s.SearchForm} onSubmit={this.handleSubmit}>
-//           <button type="submit" className={s.SearchFormButton}>
-//             <span className="button-label"></span>
-//           </button>
-
-//           <input
-//             className={s.SearchFormInput}
-//             type="text"
-//             autoComplete="off"
-//             autoFocus
-//             value={this.state.query}
-//             placeholder="Search images and photos"
-//             onChange={this.handleQueryChange}
-//           />
-//         </form>
-//       </header>
-//     );
-//   }
-// }

c84cdf039fe7677ffad6464499783d16c63a7a63
Oleh Ivanytskyi <89008107+ivanytskyi2407@users.noreply.github.com>
2023-04-11 09:14:42 +0300

    API refactoring

--- src/components/API/API.jsx
+++ src/components/API/API.jsx
@@ -2,10 +2,16 @@ import axios from 'axios';
 const API_KEY = '25715337-58cde3c0d1b1902de73779f35';
 const BASE_URL = 'https://pixabay.com/api';
 
-export default function API(name, page) {
+export default function API(query, page) {
   return axios
     .get(
-      `${BASE_URL}/?key=${API_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=12`
+      `${BASE_URL}/?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`,
+      {
+        params: {
+          limit: 12,
+          page,
+        },
+      }
     )
     .then(response => {
       return response.data;
